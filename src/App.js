127b3999498b0c338b79994cfeb5f02c17ed5316
Franco Fraiese <86889319+francofraiese@users.noreply.github.com>
2022-09-04 20:50:36 -0300

    profile terminado

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route} from 'react-router-dom'
 import Profile from './components/profile/profile';
 import Home from './components/home/home';
 import Header from './components/header/header';
+import About from './components/about/about';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <>
           <Route path='/' element={<Home/>}/>
           <Route path='/profile' element={<Profile/>}/>
+          <Route path='/about' element={<About/>}/>
         </>
       </Routes>
     </div>
--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -4,22 +4,39 @@ import avatar from '../../images/avatar1.png'
 const Profile = () => {
     return (
         <div className='Profile'>
-            <div className='ProfileAvatar'>
-                <img className='ProfileAvatarImg' src={avatar} alt='Avatar'/>
-            </div>
-            <div className='ProfileData'>
-                <div className='ProfileDataTitle'>
-                    <h1>Valentin Torres Erwerle</h1>
+            <div className='ProfileInfoTop'>
+                <div className='ProfileAvatar'>
+                    <img className='ProfileAvatarImg' src={avatar} alt='Avatar'/>
                 </div>
-                <div className='ProfileDataContent'>
-                    <div className='ProfileDataContentTop'>
-                        <h3 className='ProfileDataContentID'>ID #1</h3>
-                        <h3 className='ProfileDataContentStatus'>Diamond</h3>
+                <div className='ProfileData'>
+                    <div className='ProfileDataTitle'>
+                        <h1>Valentin Torres Erwerle</h1>
                     </div>
-                    <div className='ProfileDataContentBottom'>
-                        <h3 className='ProfileDataContentRank'>Rank #18</h3>
-                        <h3 className='ProfileDataContentPoints'>1930</h3>
+                    <div className='ProfileDataContent'>
+                        <div className='ProfileDataContentTop'>
+                            <h3 className='ProfileDataContentID'>ID #1</h3>
+                            <h3 className='ProfileDataContentStatus'>Diamond</h3>
+                        </div>
+                        <div className='ProfileDataContentBottom'>
+                            <h3 className='ProfileDataContentRank'>Rank #18</h3>
+                            <h3 className='ProfileDataContentPoints'>1930</h3>
+                        </div>
+                        <div className='ProfileDataEditDiv'>
+                            <button className='ProfileDataEditBtn'>EDIT PROFILE</button>
+                        </div>
                     </div>
+                </div>
+            </div>
+            <div className='ProfileInfoBottom'>
+                <div className='ProfileInfoBottomTitle'>
+                    <h3>Total matchs</h3>
+                    <h3>Total wins</h3>
+                    <h3>Winrate</h3>
+                </div>
+                <div className='ProfileInfoBottomData'>
+                    <h3>396</h3>
+                    <h3>217</h3>
+                    <h3>55%</h3>
                 </div>
             </div>
         </div>
