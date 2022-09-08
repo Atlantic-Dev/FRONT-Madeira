f56d2690fe5dc8da3d539d766ef4e0514f4f82ef
Franco Fraiese <86889319+francofraiese@users.noreply.github.com>
2022-09-08 01:34:04 -0300

    paginado | botones y disabled

--- src/components/list/list.jsx
+++ src/components/list/list.jsx
@@ -24,10 +24,16 @@ const List = () => {
     const indexOfFirst = indexOfLast - customersPerPage 
 
     //Cantidad de paginas
-    const pageNumbers = []
-    for (let i = 1; i <= Math.ceil(completeList?.length / customersPerPage); i++) {
+    let pageLimit = Math.ceil(completeList?.length  / customersPerPage)
+    let pageNumbers = []
+    if (currentPage < 5) {
+        pageNumbers = [1, 2, 3, 4, 5, 6, 7]
+    } else if (currentPage > (pageLimit - 4)){
+        pageNumbers = [pageLimit-6, pageLimit-5, pageLimit-4, pageLimit-3, pageLimit-2, pageLimit-1, pageLimit]
+    } else {
+    for (let i = currentPage-3; i <= currentPage+3; i++) {
         pageNumbers.push(i)
-    }
+    }}
 
     //Pagina mostrada
     const slicedList = completeList?.slice(indexOfFirst, indexOfLast)
@@ -37,41 +43,51 @@ const List = () => {
         e.preventDefault()
         setCurrentPage(parseInt(e.target.id));
     }
-    const prevPage = (e) => {
+    
+    const setPageStart = (e) => {
         e.preventDefault()
-        if(currentPage > 1) {
-            setCurrentPage(currentPage-1)}
-        }
-    const nextPage = (e) => {
+        if(currentPage > 4)setCurrentPage(1)
+    }
+
+    const setPageEnd = (e) => {
         e.preventDefault()
-        if (currentPage !== pageNumbers?.length) setCurrentPage(currentPage+1)
+        if(currentPage < pageLimit -3)setCurrentPage(pageLimit)
+    }
+
+    const setPagePrevFive = (e) => {
+        e.preventDefault()
+        if(currentPage-5 > 0)setCurrentPage(currentPage-5)
+    }
+    const setPageNextFive = (e) => {
+        e.preventDefault()
+        if(currentPage+5 < pageLimit+1)setCurrentPage(currentPage+5)
     }
     
+
+    //Renderizado de botones
     const pagination =  pageNumbers.map((p) => {
         return (             
-            <button className={currentPage === p ? "pagNactive" : "pagN"} key={p} id={p} onClick={handlePage}> {p} </button>             
+            <button className={currentPage === p ? "ListPageButtonActive" : "ListPageButton"} key={p} id={p} onClick={handlePage}> {p} </button>             
         )}
     )
 
-    
+    //Par o impar para definir className del div (diferentes BG-color)
     function isEven(n) {
         return n % 2
-    }
-    const consolelog = () => {console.log(completeList)}
-
-    function handleConsole(e){
-        e.preventDefault()
-        console.log(slicedList)
     }
 
     return(
         <div className="ListContainer">
             <div className="List">
                 <div className="ListPageButtons">
+                    <button className={currentPage > 4 ? "ListPageButton" : "ListPageButtonDisabled"} onClick={setPageStart}>1</button>
+                    <button className={currentPage > 5 ? "ListPageButton" : "ListPageButtonDisabled"} onClick={setPagePrevFive}>{currentPage-5 < 1 ? 1 : currentPage-5}</button>
+                    <div className="ListPageButtonsNumbers">
                     {pagination}
+                    </div>
+                    <button className={currentPage < pageLimit-4 ? "ListPageButton" : "ListPageButtonDisabled"} onClick={setPageNextFive}>{currentPage > pageLimit-5 ? pageLimit : currentPage+5}</button>
+                    <button className={currentPage < pageLimit-3 ? "ListPageButton" : "ListPageButtonDisabled"} onClick={setPageEnd}>{pageLimit}</button>
                 </div>
-                <button onClick={(e) => handleConsole(e)}>console</button>
-                <button onClick={consolelog}>a</button>
                 <div className="ListRanking">
                     <div className='ListRankingTitle'>
                         <div className='ListRankingTitleFirst'>
