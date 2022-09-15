5d9e3c210f306cfbec946bcd91c615bfc3c8a406
Lucia Chinni <luchinni8@gmail.com>
2022-09-15 12:01:11 -0300

    testeando login

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -6,42 +6,18 @@ describe('Website works', () => {
 
     cy.contains('players list').click()
     cy.contains('Luciano_Aufderhar').click()
-    // Should be on a new URL which
-    // includes '/commands/actions'
-    //cy.url().should('include', '/commands/actions')
-
-    // Get an input, type into it and verify
-    // that the value has been updated
-    //cy.get('.action-email')
-      //.type('fake@email.com')
-      //.should('have.value', 'fake@email.com')
+   
   })
 })
 
 describe('hay token', () => {
   it('Visits website', () => {
     cy.visit('http://react-alb-1195746012.us-east-1.elb.amazonaws.com/')
-    cy.get('login').click().should(() => {
-      
-    });
-    expect(localStorage.getItem('token'))
+    cy.get('login').click()
+    cy.get('input:first').type('luchinni8@gmail.com')
+    cy.get('input:second').type('Hola1234')
+    cy.get('login').click()
+    
+    
+    //expect(localStorage.getItem('token')) 
 })})
-
-/* let localStore;
-
-beforeEach(() => {
-  localStore = {};
-
-  cy.localStorage('getItem').and.callFake((token) =>
-    token in localStore ? localStore[token] : null
-  )
-}); */
-
-/* Visit: https://example.cypress.io
-Find the element with content: type
-Click on it
-Get the URL
-Assert it includes: /commands/actions
-Get the input with the action-email class
-Type fake@email.com into the input
-Assert the input reflects the new value */
