7f93746e9cb2f632390f30c0b76630afddf59c1a
Lucia Chinni <luchinni8@gmail.com>
2022-09-18 15:59:45 -0300

    testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,25 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect'
+import { render, screen } from '@testing-library/react'
+import { Provider } from "react-redux";
+import store from './redux/store'
+import App from './App' 
+import {BrowserRouter as Router} from 'react-router-dom'
 
-xtest('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const renderApp = component => render(
+    <Provider store={store}>
+          {component}
+        </Provider>
+)
 
 
+test('App renders', async () => {
+  renderApp(
+    <Router>
+        <App/>,
+    </Router>,
+  )
 
+  expect(screen.getByText('HOME')).toBeInTheDocument()
 
+})
